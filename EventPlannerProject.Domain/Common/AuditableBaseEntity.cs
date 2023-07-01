70339744438f0634ecb2ed584ca2ca5003ac937f
Official-Owora <chukwuemeka.owora@gmail.com>
2023-07-01 21:00:29 +0100

    Add project files.

---
+++ EventPlannerProject.Domain/Common/AuditableBaseEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventPlannerProject.Domain.Common
+{
+    public abstract class AuditableBaseEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? StaffId { get; set; }
+        public string? CreatedBy { get; set; }
+        public DateTime? DateCreated { get; set; } = DateTime.Now;
+        public string? LastModifiedBy { get; set; }
+        public DateTime? LastModifiedDate { get; set; }
+    }
+}
