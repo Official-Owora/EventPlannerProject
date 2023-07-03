6a3d73227d7bbc7b4803299871900c14a61fe682
Oluwaseyi Akinbobola <83551336+Bashorun407@users.noreply.github.com>
2023-07-03 10:53:56 +0100

    Committing newly added models and updated Repository Manager to local repository

---
+++ EventPlannerProject.Application/Contracts/INotificationRepository.cs
@@ -0,0 +1,18 @@
+﻿using EventPlannerProject.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventPlannerProject.Application.Contracts
+{
+    public interface INotificationRepository
+    {
+        Task<IEnumerable<Notification>> FindAllNotificationsAsync(bool trackChanges);
+        Task<Notification> FindNotificationById(int id, bool trackChanges);
+        void CreateNotification(Notification notification);
+        void DeleteNotification(Notification notification);
+        void UpdateNotification(Notification notification);
+    }
+}
---
+++ EventPlannerProject.Application/Contracts/IOrganizerRepository.cs
@@ -0,0 +1,18 @@
+﻿using EventPlannerProject.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventPlannerProject.Application.Contracts
+{
+    public interface IOrganizerRepository
+    {
+        Task<IEnumerable<Organizer>> FindAllOrganizersAsync(bool trackChanges);
+        Task<Organizer> FindOrganizerById(int id, bool trackChanges);
+        void CreateOrganizer(Organizer organizer);
+        void UpdateOrganizer(Organizer organizer);
+        void DeleteOrganizer(Organizer organizer);
+    }
+}
