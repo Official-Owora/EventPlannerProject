8a6e1891bcae5687d6789a18712f9f6b637020b4
Oluwaseyi Akinbobola <83551336+Bashorun407@users.noreply.github.com>
2023-07-04 11:19:14 +0100

    Committing updated files to local repository

--- EventPlannerProject.Domain/Models/Events.cs
+++ EventPlannerProject.Domain/Models/Events.cs
@@ -34,12 +34,13 @@ namespace EventPlannerProject.Domain.Models
 
         //Navigational properties
         //Has One to Many mapping with Assignment table
-        public ICollection<Assignment>? Assignment { get; set; }
+        List<IEnumerable<Assignment>>? Assignment { get; set; }
 
         //Has One to Many mapping with Participant table
-        public ICollection<Participant>? Participant { get; set; }
+        List<IEnumerable<Participant>>? Participant { get; set; }
 
         //Has One to Many mapping with Notification
-        public ICollection<Notification>? Notification { get; set; }
+        List<IEnumerable<Notification>>? Notification { get; set; }
+
     }
 }
