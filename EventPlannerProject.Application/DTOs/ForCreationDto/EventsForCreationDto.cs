589c64dcbd80199ac39b320ee410bc563a8ab2e4
Official-Owora <chukwuemeka.owora@gmail.com>
2023-07-04 09:44:35 +0100

    Commit cchanges made in the Application Layer

--- EventPlannerProject.Application/DTOs/ForCreationDto/EventsForCreationDto.cs
+++ EventPlannerProject.Application/DTOs/ForCreationDto/EventsForCreationDto.cs
@@ -1,38 +1,36 @@
-﻿using EventPlannerProject.Domain.Common;
+﻿using EventPlannerProject.Domain.Models;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EventPlannerProject.Domain.Models
+namespace EventPlannerProject.Application.DTOs.ForCreationDto
 {
-    public class Events : AuditableBaseEntity
+    public class EventsForCreationDto
     {
-        [Required(ErrorMessage ="Event description is required")]
-        [MaxLength(100, ErrorMessage ="Maximum length for this description is 100 characters")]
+        [Required(ErrorMessage = "Event description is required")]
+        [MaxLength(100, ErrorMessage = "Maximum length for this description is 100 characters")]
         public string? EventDescription { get; set; }
 
-        [Required(ErrorMessage ="Date of the event is required")]
+        [Required(ErrorMessage = "Date of the event is required")]
         public DateTime? EventStartDate { get; set; }
 
-        [Required(ErrorMessage ="Time of the event is required")]
+        [Required(ErrorMessage = "Time of the event is required")]
         public string? EventStartTime { get; set; }
 
         [Required(ErrorMessage = "Venue of the event is required")]
         public string? EventVenue { get; set; }
 
         [Required(ErrorMessage = "Address of the event is required")]
-        [MaxLength(70, ErrorMessage ="Maximum leength for this address is 50 characters")]
+        [MaxLength(50, ErrorMessage = "Maximum leength for this address is 50 characters")]
         public string? EventAddress { get; set; }
 
-        [Required(ErrorMessage ="The state event will happen is required"), DataType(DataType.Text)]
-        [MaxLength(20, ErrorMessage ="Maximum length for the state is 20 characters")]
+        [Required(ErrorMessage = "The state event will happen is required"), DataType(DataType.Text)]
+        [MaxLength(20, ErrorMessage = "Maximum length for the state is 20 characters")]
         public string? EventLocationByState { get; set; }
 
-        //Navigational properties
         //Has One to Many mapping with Assignment table
         public ICollection<Assignment>? Assignment { get; set; }
 
---
+++ EventPlannerProject.Application/DTOs/ForDisplayDto/ParticipantForDisplayDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventPlannerProject.Application.DTOs.ForDisplayDto
+{
+    public class ParticipantForDisplayDto
+    {
+        public int Id { get; set; }
+        public string? ParticipantFirstName { get; set; }
+        public string? ParticipantLastName { get; set; }
+        public string? ParticipantEmailAddress { get; set; }
+        public string? ParticipantPhoneNumber { get; set; }
+        public string? RSVPStatus { get; set; } 
+    }
+}
