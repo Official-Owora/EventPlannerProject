48f534954a9fbb032f70d853e0c0baeaf4006a77
Oluwaseyi Akinbobola <83551336+Bashorun407@users.noreply.github.com>
2023-07-04 16:14:59 +0100

    Committing all Repositories in Infrastructure to local repository

---
+++ EventPlannerProject.Persistence/Repositories/NotificationRepository.cs
@@ -0,0 +1,49 @@
+﻿using EventPlannerProject.Application.Contracts;
+using EventPlannerProject.Domain.Models;
+using EventPlannerProject.Persistence.Common;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventPlannerProject.Persistence.Repositories
+{
+    internal sealed class NotificationRepository : RepositoryBase<Notification>, INotificationRepository
+    {
+        public NotificationRepository(RepositoryContext repositoryContext) : base(repositoryContext)
+        {
+
+        }
+
+        public void CreateNotification(Notification notification)
+        {
+            CreateNotification(notification);
+        }
+
+        public void DeleteNotification(Notification notification)
+        {
+            DeleteNotification(notification);
+        }
+
+        public async Task<IEnumerable<Notification>> FindAllNotificationsAsync(bool trackChanges)
+        {
+            return await FindAllAsync(trackChanges)
+                .OrderBy(x=> x.Id)
+                .ToListAsync();
+        }
+
+        public async Task<Notification> FindNotificationById(int id, bool trackChanges)
+        {
+            var notification = await FindByConditionAsync(x => x.Id == id, trackChanges).ToListAsync();
+
+            return notification.FirstOrDefault();
+        }
+
+        public void UpdateNotification(Notification notification)
+        {
+            Update(notification);
+        }
+    }
+}
---
+++ EventPlannerProject.Persistence/Repositories/OrganizerRepository.cs
@@ -0,0 +1,50 @@
+﻿using EventPlannerProject.Application.Contracts;
+using EventPlannerProject.Domain.Models;
+using EventPlannerProject.Persistence.Common;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventPlannerProject.Persistence.Repositories
+{
+    internal sealed class OrganizerRepository : RepositoryBase<Organizer>, IOrganizerRepository
+    {
+        public OrganizerRepository(RepositoryContext repositoryContext) : base(repositoryContext)
+        {
+
+        }
+
+        public void CreateOrganizer(Organizer organizer)
+        {
+            Create(organizer);
+        }
+
+        public void DeleteOrganizer(Organizer organizer)
+        {
+            Delete(organizer);
+        }
+
+        public async Task<IEnumerable<Organizer>> FindAllOrganizersAsync(bool trackChanges)
+        {
+            return await FindAllAsync(trackChanges)
+                .OrderBy(x=> x.Id)
+                .ToListAsync();
+        }
+
+        public async Task<Organizer> FindOrganizerById(int id, bool trackChanges)
+        {
+            var organizer = await FindByConditionAsync(x=> x.Id == id, trackChanges)
+                .ToListAsync();
+
+            return organizer.FirstOrDefault();
+        }
+
+        public void UpdateOrganizer(Organizer organizer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
