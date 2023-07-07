3829a884c4efeaff2cca89361de594a1226dd886
Official-Owora <chukwuemeka.owora@gmail.com>
2023-07-07 13:58:40 +0100

    commit changes to repository

---
+++ EventPlannerProject.ServiceContract/Interfaces/IServiceManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventPlannerProject.ServiceContract.Interfaces
+{
+    public interface IServiceManager
+    {
+        IAssignmentService AssignmentService { get; }
+        IEventsService EventsService { get; }
+        IParticipantService ParticipantService { get; }
+    }
+}
