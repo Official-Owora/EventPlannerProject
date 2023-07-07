ebdb122df520872d90a5387255e8923b7d8a3dbb
Oluwaseyi Akinbobola <83551336+Bashorun407@users.noreply.github.com>
2023-07-07 18:24:31 +0100

    Committing all updated files to local repository

---
+++ EventPlannerProject.ServiceRepository/Service/ServiceManager.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using EventPlannerProject.Application.Common;
+using EventPlannerProject.ServiceContract.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventPlannerProject.ServiceRepository.Service
+{
+    public sealed class ServiceManager : IServiceManager
+    {
+        private readonly Lazy<IAssignmentService> _assignmentService;
+        private readonly Lazy<IEventsService> _eventsService;
+        private readonly Lazy<INotificationService> _notificationService;
+        private readonly Lazy<IOrganizerService> _organizerService;
+        private readonly Lazy<IParticipantService> _participantService;
+
+        public ServiceManager(IRepositoryManager repositoryManager, ILoggerManager loggerManager, IMapper mapper)
+        {
+            _notificationService = new Lazy<INotificationService>(() => new NotificationService(repositoryManager, loggerManager, mapper));
+            _organizerService = new Lazy<IOrganizerService>(() => new OrganizerService(repositoryManager, loggerManager, mapper));
+        }
+
+        public IAssignmentService AssignmentService => throw new NotImplementedException();
+
+        public IEventsService EventsService => throw new NotImplementedException();
+
+        public IParticipantService ParticipantService => throw new NotImplementedException();
+
+        public INotificationService NotificationService => _notificationService.Value;
+
+        public IOrganizerService OrganizerService => _organizerService.Value;
+    }
+}
