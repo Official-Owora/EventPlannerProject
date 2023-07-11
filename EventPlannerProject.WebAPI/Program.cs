32f47d4346ee837d19e4adff73926a55a7d2078b
Oluwaseyi Akinbobola <83551336+Bashorun407@users.noreply.github.com>
2023-07-11 09:49:24 +0100

    Committing updated Program.cs to local repository

--- EventPlannerProject.WebAPI/Program.cs
+++ EventPlannerProject.WebAPI/Program.cs
@@ -1,6 +1,17 @@
+using EventPlannerProject.Application.Common;
+using EventPlannerProject.WebAPI.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
+builder.Services.ConfigureLoggerService();
+builder.Services.ConfigureRepositoryManager();
+builder.Services.ConfigureServiceManager();
+builder.Services.ConfigureSQLContext(builder.Configuration);
+builder.Services.AddAutoMapper(typeof(Program));
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -9,14 +20,30 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+//Configure the HTTP Pipeline for the global error handling
+var logger = app.Services.GetRequiredService<ILoggerManager>();
+app.ConfigureExceptionHandler(logger);
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+else if(app.Environment.IsProduction())
+{
+    app.UseHsts();
+}
 
 app.UseHttpsRedirection();
+app.UseStaticFiles(); //Handles HTML files
+
+app.UseForwardedHeaders( new ForwardedHeadersOptions
+{
+    ForwardedHeaders = Microsoft.AspNetCore.HttpOverrides.ForwardedHeaders.All
+});
+
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
